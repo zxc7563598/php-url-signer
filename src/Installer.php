d0533577392f2c4ea2e1ea4e9c6714989ead6586
苏青安 <992182040@qq.com>
2025-04-19 10:26:06 +0800

    一些零碎的小毛病调整

--- src/Installer.php
+++ src/Installer.php
@@ -9,27 +9,23 @@ class Installer
      * 
      * @return void 
      */
-    public static function generateKey()
+    public static function generateKey(): array
     {
         $configDir = __DIR__ . '/../config';
         $configFile = $configDir . '/urlsigner.php';
-
         // 确保 config 目录存在
         if (!is_dir($configDir)) {
             mkdir($configDir, 0755, true);
         }
-
-        // 如果已有密钥，则跳过
+        // 如果已有密钥，则返回
         if (file_exists($configFile)) {
             $config = include $configFile;
             if (!empty($config['secretKey'])) {
-                return;
+                return $config;
             }
         }
-
         // 生成一个 32 字符长度的随机密钥
         $key = bin2hex(random_bytes(16)); // 16 字节 = 32 十六进制字符
-
         // 写入配置文件
         $content = <<<PHP
 <?php
@@ -41,5 +37,10 @@ return [
 PHP;
 
         file_put_contents($configFile, $content);
+        $config = include $configFile;
+        if (!empty($config['secretKey'])) {
+            return $config;
+        }
+        throw new \Exception('配置文件生成失败，请手动传入配置信息');
     }
 }
